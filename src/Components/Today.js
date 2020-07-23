4d4a393170e36dc5d7eccefcde7589a781da19ae
Camila Fragoso <camilafragoso.98@hotmail.com>
2020-07-23 11:51:22 -0300

    Selector working

--- src/Components/Today.js
+++ src/Components/Today.js
@@ -1,35 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Components/Today.css';
 import Typography from '@material-ui/core/Typography';
 
 
 const Today = (props)=> {
 
+    const [chosenSun, setChosenSun] = useState();
+
     let item = null; 
     let setkey = null;
 
     let options = Object.keys(props.untreatedData).map((key, index) => {
-        if (key.length == 3 && index < 6) {
+        if (key.length == 3 && index < 7) {
             item = props.untreatedData[key];
             setkey = key;
         } else return;
-        return (<option>{new Date(item.Last_UTC).toDateString()}</option>);
+    return (<option value={key}>Sun {key}</option>);
     });
+
+
+    const handleSelectChange = (sun) => {
+        Object.keys(props.untreatedData).map((key, index) => {
+            if (key.length == 3) {
+                if (sun === key){
+                    setChosenSun(key);
+                    props.sendData(key);
+                }
+            } else return;
+        })
+    };
 
 
 
     return (
         <div className="all">
             <div className="blockdifferent">
-                <select className="picker" onChange={(event)=> {props.handleSelectChange(event.target.value)}}>
-                    <option className="option" value={props.lastutc}>{props.lastutc}</option>
+                <select className="picker" onChange={(event)=> {handleSelectChange(event.target.value)}}>
+                    <option className="option" value={props.sun}>Sun {props.sun}</option>
                     {options}
                 </select>
             </div>
             <div className='today'>
             <div className="block">
-                <Typography  className="text"gutterbottom>Sun</Typography>
-                <Typography className="text"variant="h6" className="title">{props.sun}</Typography>
+                <Typography  className="text"gutterbottom>Date</Typography>
+                <Typography className="text"variant="h6" className="title">{props.lastutc}</Typography>
             </div>
             <div className="block">
                 <Typography  className="text"gutterbottom>Temperature</Typography>
--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -8,6 +8,7 @@ import Today from '../Components/Today';
 const MainContainer = () => {
 
     const [untreatedData, setUntreatedData] = useState([]);
+    const [theKey, setTheKey] = useState();
     const [directions, setDirections] = useState([["N", "North"], ["S", "South"], ["W", "West"], ["E", "East"], ["NE", "Northeast"], ["SE", "Southeast"], ["NW", "Northwest"], ["SW", "Southwest"], ['NNE', 'North-Northeast'], ['ENE', 'East-Northeast'], ['ESE', 'East-Southeast'], ['SSE', 'South-Southeast'], ['SSW', 'South-Southwest'], ['WSW', 'West-Southwest'], ['WNW', 'West-Northwest'], ['NNW', 'North-Northwest']]);
 
     //fetching data from the api and turning into array
@@ -21,39 +22,67 @@ const MainContainer = () => {
         });
     });   
 
+    let getData = (key) => {
+        setTheKey(key);
+    }
+
+    let withkey = null; 
+
+    if (theKey !== undefined){
+        withkey = 
+        <Today sun={theKey} at={untreatedData[theKey].AT.av} hws={(untreatedData[theKey].HWS.av)*(3,6)}
+        pre={untreatedData[theKey].PRE.av} season={untreatedData[theKey].Season} untreatedData={untreatedData}
+        firstutc={new Date(untreatedData[theKey].First_UTC).toDateString()}
+        lastutc={new Date(untreatedData[theKey].Last_UTC).toDateString()}
+        sendData={getData}
+        //converting the wind direction to its corresponding name
+        wd={directions.map(direction => {
+                if (untreatedData[theKey].WD.most_common.compass_point === direction[0]){
+                    return (direction[1]);
+                } else return;
+            })
+        }/>;
+    }
+
     //rendering the data dinamically
-    let item = null; 
     let setkey = null;
+    let item = null;
 
-    let teste = Object.keys(untreatedData).map((key, index) => {
+    let firstsun = Object.keys(untreatedData).map((key, index) => {
 
-        //confirming if the data is valid and printing the most recent one
-        if (key.length == 3 && index == 6) {
-            item = untreatedData[key];
-            setkey = key;
-        } else return;
-        
-        return (
-            <Today sun={setkey} at={item.AT.av} hws={(item.HWS.av)*(3,6)}
-                pre={item.PRE.av} season={item.Season} untreatedData={untreatedData}
-                firstutc={new Date(item.First_UTC).toDateString()}
-                lastutc={new Date(item.Last_UTC).toDateString()}
+        if (theKey == undefined){
+            //confirming if the data is valid and printing the most recent one
+            if (key.length == 3 && index == 6) {
+                item = untreatedData[key];
+                setkey = key;
+            } else return;
 
-                //converting the wind direction to its corresponding name
-                wd={directions.map(direction => {
-                        if (item.WD.most_common.compass_point === direction[0]){
-                            return (direction[1]);
-                        } else return;
-                    })
-                }/>
-        );
+            return (
+                <Today sun={setkey} at={item.AT.av} hws={(item.HWS.av)*(3,6)}
+                    pre={item.PRE.av} season={item.Season} untreatedData={untreatedData}
+                    firstutc={new Date(item.First_UTC).toDateString()}
+                    lastutc={new Date(item.Last_UTC).toDateString()}
+                    sendData={getData}
+
+                    //converting the wind direction to its corresponding name
+                    wd={directions.map(direction => {
+                            if (item.WD.most_common.compass_point === direction[0]){
+                                return (direction[1]);
+                            } else return;
+                        })
+                    }/>
+            );
+
+        } else return; 
       });
 
+    
                         
     return (
         <div className="maincontainer">
             <h1>Weather on Mars</h1>
-            {teste}
+            {firstsun}
+            {withkey}
             <video id="background-video" loop autoPlay muted>
                 <source src={Mars} type="video/mp4" />
             </video>
